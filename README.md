3375f0ea92ff08eaf130ef48b93a77c39312f88c
wangchang <wangchang@didichuxing.com>
2017-08-03 19:16:59 +0800

    add test command

--- README.md
+++ README.md
@@ -3,37 +3,64 @@ scp uploader
 
 uploader scripts using scp
 
-### config
+### 配置
 
-see details in build/deploy.js
+```javascript
+
+const config = {
+        //自己的 docker Ip
+        ip: '10.95.156.161',
+        from: 'dist/',
+        //远端目录，一般是nginx根目录
+        to: '/usr/local/webserver/nginx/html',
+        //上传到远程后的文件目录
+        remoteDirName : 'passenger',
+        //[暂不支持]不发布widget文件夹
+        exclude : /\/widget\//i,
+        //[暂不支持]文件进行字符串替换，可以先run test
+        replace : {
+            from : 'static.udache.com',
+            to : '10.95.156.161'
+        }
+};
+
+```
 
 
-### dependency
+### 依赖
 
 ```bash
 $ npm install archiver
 ```
 
-### use
-you must add SSH keys to your docker before use,see [SSH keys help](https://itbilu.com/linux/management/EJTmGG7re.html)
+### 使用
+
+1. 使用前需要在虚拟机上添加SSH keys，[SSH 帮助](https://itbilu.com/linux/management/EJTmGG7re.html)，这一步是用来进行登陆认证的，如果已经添加过，可以跳过
+
+2. copy deploy.js脚本到自己的build目录下，改一下配置config，然后
 
 ```bash
-$ node deploy.js # in project you can decide on youself
+node ./build/deploy.js
 ```
 
-### use in project
-1. modify the domain in config/test.js
-
-2. modify the config in build/deploy.js
-
-3. add npm scripts in package.json
+3. 也可以在package.json中添加scripts
 
 ```javascript
     "scripts":{
         "deploy": "node ./build/deploy.js"
     }
 ```
-4. npm run deploy
+
+然后执行
+
+```
+$ npm run test  # 测试时先生成测试代码
+
+$ npm run deploy # 将dist上传到虚拟机
+
+$ npm run deploy "price/price-rule" ## 先执行了 npm run test "price/price-rule" 然后deploy
+
+```
 
 
 
--- build/deploy.js
+++ build/deploy.js
@@ -2,35 +2,47 @@ var fs = require('fs');
 var archiver = require('archiver');
 var path = require("path");
 var child_process = require("child_process");
-var cwd = process.cwd()
+var cwd = process.cwd();
+var project = process.argv[2] //price/fee-detail
 
 const config = {
-        ip: '10.95.156.161',
-        from: 'dist/',
-        //远端目录
-        to: '/usr/local/webserver/nginx/html',
-        //上传到远程后的文件目录
-        remoteDirName : 'passenger',
-        //widget目录下的那些文件就不要发布了
-        exclude : /\/widget\//i,
-        //支持对文件进行字符串替换
-        replace : {
-            from : 'static.udache.com',
-            to : '10.95.156.161'
-        }
+    ip: '10.95.156.161',
+    from: 'dist/',
+    //远端目录
+    to: '/usr/local/webserver/nginx/html',
+    //上传到远程后的文件目录,可以根据线上实际路径来处理
+    remoteDirName : 'driver',
+    //[暂不支持]不发布widget文件夹
+    exclude : /\/widget\//i,
+    //[暂不支持]文件进行字符串替换，可以先run test
+    replace : {
+        from : 'static.udache.com',
+        to : '10.95.156.161'
+    }
 };
 
 init();
 
 function init(){
-    console.info('deploying to: '+config.ip)
+    console.info('deploying to: '+config.ip);
+    if(project){
+        var test_cmd = 'npm run test '+project;
+        try{
+            var data = child_process.execSync(test_cmd);
+            console.log('test task for: '+project+' complete');
+        }catch (e){
+            console.log(e);
+        }
+    }
     compress(config,function(){
         runUpload(config)
     })
+
 }
 
 
 function compress(config,cb){
+
     // create a file to stream archive data to.
     var output = fs.createWriteStream(cwd+'/pack.zip');
 
@@ -45,7 +57,6 @@ function compress(config,cb){
 // callbacks
     output.on('close', function() {
         cb();
-        console.log(archive.pointer() + ' total bytes archived');
     });
     archive.on('error', function(err) {
         throw err;
@@ -56,9 +67,9 @@ function compress(config,cb){
 
 function runUpload(config) {
     if (!config.to) {
-        throw new Error('options.to is required!');
+        throw new Error('config.to is required!');
     } else if (!config.remoteDirName) {
-        throw new Error('options.remoteDirName is required!');
+        throw new Error('config.remoteDirName is required!');
     }
     var zipfile = 'pack.zip';
     var scp_cmd = 'scp '+zipfile+' root@'+config.ip+':'+config.to+';rm -rf '+ zipfile +';';
